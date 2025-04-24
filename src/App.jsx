8d030cbacf5c26d629a56c95a46166a97fe4de14
Ajay Pratap Chand <apchand99@gmail.com>
2025-04-25 00:36:14 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
  import './App.css'
 import WeatherApp from "./WeatherApp"
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 function App() {
   return (<div style={{textAlign:"center"}}>
     <WeatherApp></WeatherApp>
+    <h3>Made with <FavoriteIcon /> By Ajay</h3>
    </div>)
 }
 
